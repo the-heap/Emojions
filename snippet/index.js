7ad76cefd79dd2a6fac73264a996e687fe27b8cc
teesloane <tylersloane@gmail.com>
2017-04-28 22:19:47 -0400

    Code Session 1: Pipe Scaffolding
    - Quite a bit of work done here to scaffold
    out the basic structure and first few functions
    of our function pipe.
    
    - Made an example page to mount the emojion bar in
    - Created the function pipe (needs work!)
    - Made a state object to pass through the pipe and alter as it flows down
    - Made our own custom containers for mounting the emoji bar in and made them show up.

---
+++ snippet/index.js
@@ -0,0 +1,135 @@
+~(function () {
+
+  // =====================================================
+  // SETUP (vars, conts, function pipe)
+  // =====================================================
+
+  // Constants
+  EMOJION_ID = 'emojion'
+
+  const EMOJION_STAMP = () => [
+    { icon: '😅', count: 0 },
+    { icon: '🗻', count: 0 },
+    { icon: '⚓', count: 0 },
+    { icon: '🌵', count: 0 }
+  ]
+
+
+  // State to pass through function pipeline
+  var state = {
+    dom: { mounts: [], containers: [] },
+    emojis : {}
+  }
+
+  // The All Magical, Beautiful Function Pipe!
+
+  /**
+   * create a function pipe.
+   * TODO: refactor to be more readable. PLEASE.
+   * @param {functions} funs -> unknown number of functions
+   * @returns - a state object. 
+   */
+  function pipe(...funs) {
+    return funs.reduce(
+      (f, g) => (...args) => g(f(...args))
+    )
+  }
+
+
+  // =====================================================
+  // THE FUNCTION PIPE IN ACTION!!!
+  // =====================================================
+
+  const render = pipe(
+    getAllIds,
+    makeEmojionBars,
+    makeContainers,
+    populateContainers
+  )(state)
+  console.log(state)
+
+
+  // =====================================================
+  // Create the functions that will feed into the pipe.
+  // =====================================================
+
+  /**
+   * Get all the ids from the page, 
+   * filter out the ones we want to mount emojion bar on
+   *  
+   * @param {object} state 
+   * @returns {object} state
+   */
+  function getAllIds(state) {
+    const ids = [...document.querySelectorAll('[id]')]; // -> converts nodelist to array
+    const mounts = ids.filter(node => node.id.includes(EMOJION_ID));
+    state.dom.mounts = mounts
+    return state
+  }
+
+  /**
+   * Loop through the dom elements that need an emojion bar
+   * Generate a new emojio stamp for that dom element. 
+   * @param {obj} state 
+   * @returns {obj} state - now with emoji structures
+   */
+  function makeEmojionBars(state) {
+    state.dom.mounts.forEach(mount => {
+      state.emojis[mount.id] = EMOJION_STAMP()
+    })
+    return state
+  }
+
+
+  /**
+   * Create our own dom element to actually mount our emojion bar in
+   * This is different than the mounts that user specifices for where they want the bar.
+   * TODO: Rename this.
+   * @param {obj} state 
+   * @returns {obj} state - our custom dom containers for our emoji bars
+   */
+  function makeContainers(state){
+    state.dom.mounts.forEach(mount => {
+      // make our element + attributes
+      let emojiContainer = document.createElement('DIV');
+      let containerClass = document.createAttribute('class');
+      const containerMapId = document.createAttribute('data_map_id');
+
+      // set a value on our html attribute (ie. class = " emojion_container") -> add to dom element
+      containerClass.value = "emojion_container"
+      containerMapId.value = mount.id
+      emojiContainer.setAttributeNode(containerClass)
+      emojiContainer.setAttributeNode(containerMapId)
+
+      mount.appendChild(emojiContainer)
+      state.dom.containers.push(emojiContainer)
+
+    })
+    return state
+
+  }
+
+  
+  /**
+   * Go through all of OUR containers, and fill out the emojion bar 
+   * Pair Dom elements up with data structure so they receive the correct emojion bar 
+   * @param {any} state 
+   * @returns 
+   */
+  function populateContainers(state) {
+    state.dom.containers.forEach(container => {
+      const id = container.attributes.data_map_id.value
+      // create an html TEMPLATE, and display it.
+      container.innerHTML = state.emojis[id].map((emoji, index) => {
+        return `
+          <div class="emojion_single">
+            ${emoji.icon} ${emoji.count}
+          </div>
+        `
+      }).join('') // remove commas between elements
+    })
+    return state
+  }
+
+
+})()
