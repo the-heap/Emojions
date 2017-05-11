b3d41b535cd70ef30535c3a877cbf70f7a418a4c
teesloane <tylersloane@gmail.com>
2017-05-10 21:36:29 -0400

    basic elm hackery. Making a header for the client!

--- client/main.elm
+++ client/main.elm
@@ -5,65 +5,36 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 
-main : Program Never Model Msg
+-- main : Program Never Model Msg
+
+
 main =
-    Html.beginnerProgram { model = model, view = view, update = update }
-
-
-{- Let's define our model! We need the following:
-   A type alias
-   To set the type to the function "model"
-   To actually write the model function
- -}
-
-
-type alias Model =
-    { buttonMessage : String
-    , buttonOn : Bool
-    }
-
-
-model : Model
-model =
-    { buttonMessage = "I am on"
-    , buttonOn = False
-    }
+    Html.beginnerProgram { model = 0, update = update, view = view }
 
 
 
-{-Update Section. We need the following:
-    * A union type for listing our actions
-    * Set the update function have a type
-    * To write the update function
--}
+-- redundant
 
 
 type Msg
-    = ToggleButton
+    = Increment
 
 
-update : Msg -> Model -> Model
+-- redundant
 update msg model =
     case msg of
-        ToggleButton ->
-            { model
-                | buttonOn = not model.buttonOn
-                , buttonMessage = createButtonText model.buttonOn
-            }
+        Increment ->
+            model + 1
 
 
-view : Model -> Html Msg
+
 view model =
-    button [ onClick ToggleButton ] [ text model.buttonMessage ]
-
-
-
--- LITTLE FUNCTIONS THAT MAKE UP OUR GOOD LIFE --
-
-
-createButtonText : Bool -> String
-createButtonText boolState =
-    if boolState then
-        "I am on"
-    else
-        "I am off"
+    div []
+        [ header [ class "header" ]
+            [ h1 [ class "header__text" ]
+                [ text "Emojion Bar" ]
+            , p []
+                [ text "Custom Emoji Reaction Bars for your sweet site." ]
+            ]
+        , Html.node "link" [ Html.Attributes.rel "stylesheet", Html.Attributes.href "style.css" ] []
+        ]
