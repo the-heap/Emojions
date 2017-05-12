33cf053267a2e96f1072663d289bbfa51b6c577b
Gilbert Kennen <gilbertkennen@users.noreply.github.com>
2017-05-12 13:55:22 -0700

    Use native elm-css for type-safe CSS. (#42)
    * Add elm-live development server support.
    
    * Replaced hardcoded css with elm-css. Also renamed main.elm to Main.elm (Elm
    standard) and cleaned it up a bit.

---
+++ client/Style.elm
@@ -0,0 +1,61 @@
+module Style exposing (..)
+
+import Css
+    exposing
+        ( Mixin
+        , alignItems
+        , backgroundColor
+        , center
+        , color
+        , column
+        , displayFlex
+        , flexDirection
+        , fontFamilies
+        , fontSize
+        , justifyContent
+        , margin
+        , padding
+        , padding2
+        , pct
+        , px
+        , rgb
+        , sansSerif
+        , zero
+        )
+
+
+body : List Mixin
+body =
+    [ fontSize (px 12)
+    , fontFamilies [ "Arial", "Helvetica", .value sansSerif ]
+    ]
+
+
+header : List Mixin
+header =
+    [ displayFlex
+    , justifyContent center
+    , alignItems center
+    , flexDirection column
+    , backgroundColor (rgb 41 128 185)
+    , color (rgb 236 240 241)
+    , padding2 (Css.rem 1) zero
+    ]
+
+
+header__text : List Mixin
+header__text =
+    [ fontSize (Css.rem 3)
+    ]
+
+
+html : List Mixin
+html =
+    [ fontSize (pct 62.5) ]
+
+
+hs : List Mixin
+hs =
+    [ margin zero
+    , padding zero
+    ]
