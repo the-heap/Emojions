e196045968db3117159b24400efd145a29005fde
Tyler <teesloane@users.noreply.github.com>
2017-05-21 22:51:28 -0400

    Styling/dummy emojis (#61)
    * Replace dummy text with dummy emojis.
    
    This also sets up somre more comments + formatting.
    
    * Styling the emoji icon size
    
    * Add more emojis + style
    
    * renaming
    
    * fix broken snippet link

--- client/src/Style.elm
+++ client/src/Style.elm
@@ -1,27 +1,6 @@
 module Style exposing (..)
 
-import Css
-    exposing
-        ( Mixin
-        , alignItems
-        , backgroundColor
-        , center
-        , color
-        , column
-        , displayFlex
-        , flexDirection
-        , fontFamilies
-        , fontSize
-        , justifyContent
-        , margin
-        , padding
-        , padding2
-        , pct
-        , px
-        , rgb
-        , sansSerif
-        , zero
-        )
+import Css exposing (..)
 
 
 body : List Mixin
@@ -57,5 +36,55 @@ html =
 hs : List Mixin
 hs =
     [ margin zero
-    , padding zero
+    ]
+
+
+{-| Style the size of a single emoji
+Takes a float to decide the font size
+-}
+emojionView : Float -> List Mixin
+emojionView size =
+    [ fontSize (px size)
+    , listStyleType none
+    ]
+
+availableView : List Mixin
+availableView =
+    [ displayFlex
+    , justifyContent center
+    , maxWidth (pct 40)
+    , flexWrap wrap
+    , margin2 (zero) (auto)
+    ]
+
+
+{-| Style the li's that contain an emoji
+-}
+availableLi : List Mixin
+availableLi =
+    [ listStyleType none
+    , justifyContent center
+    , padding (px 5)
+    , cursor pointer
+    ]
+
+
+selectedView : List Mixin
+selectedView =
+    [ displayFlex
+    , justifyContent center
+    , alignItems center
+    , minHeight (px 150)
+    , padding (px 0)
+    ]
+
+
+selectedLi : List Mixin
+selectedLi =
+    [ listStyleType none
+    , flexDirection column
+    , padding (px 20)
+    , displayFlex
+    , textAlign center
+    , cursor pointer
     ]
