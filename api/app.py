ae1bd88f8c32ae892cb988ede877545baf417949
teesloane <tylersloane@gmail.com>
2017-05-22 21:19:38 -0400

    Remove app.py lint errors.

--- api/app.py
+++ api/app.py
@@ -1,20 +1,24 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
 import json
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
+# Initialize flask and enable CORS support.
+APP = Flask(__name__)
+CORS(APP)
 
 # Temporary dummy database
-db = json.loads(open('snippet.json', 'r', encoding='utf-8').read())
+DB = json.loads(open('snippet.json', 'r', encoding='utf-8').read())
 
-@app.route('/getEmojisCount', methods=['GET'])
-def getEmojis():
+
+@APP.route('/getEmojisCount', methods=['GET'])
+def get_emojis():
     ''' Returns emoji count '''
-    return jsonify(db)
+    return jsonify(DB)
 
-@app.route('/saveEmojis', methods=['POST'])
-def saveEmojis():
+
+@APP.route('/saveEmojis', methods=['POST'])
+def save_emojis():
+    '''Saves emoji data to a DB (in this case a flat json file'''
     if request.method == 'POST':
         value = request.get_json()
         with open('snippet.json', 'w', encoding='utf-8') as jsondata:
@@ -22,5 +26,4 @@ def saveEmojis():
         return jsonify('Successful parse!')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    APP.run(debug=True)
