00f207b325097b9f5267b0a574e2b41f4bab71d1
Andrew <andrewhoang93@gmail.com>
2017-05-22 20:23:51 -0400

    Took json data and dumped into snippet db

--- api/app.py
+++ api/app.py
@@ -13,28 +13,13 @@ def getEmojis():
     ''' Returns emoji count '''
     return jsonify(db)
 
-
 @app.route('/saveEmojis', methods=['POST'])
 def saveEmojis():
-    # Code stud TODO
     if request.method == 'POST':
         value = request.get_json()
-        for key in value:
-            print(db[key])
-
-        '''Need to access the variables inside each 'key' variable '''
+        with open('snippet.json', 'w', encoding='utf-8') as jsondata:
+            json.dump(value, jsondata)
         return jsonify('Successful parse!')
-
-''' Take front end data, compare to backend data'''
-
-'''
-- Open json file
-- Validate if the data exists already
-- if not():
-- append to end of json file
-- else:
-- update to unique ID
-'''
 
 if __name__ == '__main__':
     app.run(debug=True)
